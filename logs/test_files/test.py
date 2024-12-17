ef9e39cbbb463848a5ed2cfd8597a71271d5e559
RAPALLE2 <152060617+RAPALLE2@users.noreply.github.com>
2024-12-17 15:31:02 +0100

    selection made in test.py

---
+++ logs/test_files/test.py
@@ -0,0 +1,35 @@
+def ask():
+    while True:
+        current = "2"
+        if current == "1":
+            print("(X) Option 1")
+            print("( ) Option 2")
+            print("( ) Option 3")
+            print("( ) Option 4")
+            print("( ) Option 5")
+        elif current == "2":
+            print("( ) Option 1")
+            print("(X) Option 2")
+            print("( ) Option 3")
+            print("( ) Option 4")
+            print("( ) Option 5")
+        elif current == "3":
+            print("( ) Option 1")    
+            print("( ) Option 2")
+            print("(X) Option 3")
+            print("( ) Option 4")
+            print("( ) Option 5")
+        elif current == "4":
+            print("( ) Option 1")
+            print("( ) Option 2")
+            print("( ) Option 3")
+            print("(X) Option 4")
+            print("( ) Option 5")
+        elif current == "5":
+            print("( ) Option 1")
+            print("( ) Option 2")
+            print("( ) Option 3")
+            print("( ) Option 4")
+            print("(X) Option 5")
+        break    
+ask()
