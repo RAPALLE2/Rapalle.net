aff5ea6617143d7fb824a7ceecd0a48b7f26f251
RAPALLE2 <minecrafter_tobi@web.de>
2024-12-18 16:42:37 +0100

    added mod Utilman, added Instance, imported Servers

---
+++ addons/Minecraft/Instance/src/client/java/com/rapalle/net/ClientClient.java
@@ -0,0 +1,10 @@
+package com.rapalle.net;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
