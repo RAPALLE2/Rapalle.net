b5d235c0ace9d61ab779ec54dedc289d4dd80a4f
RAPALLE2 <minecrafter_tobi@web.de>
2024-12-19 19:58:53 +0100

    MysqlManager.py

--- database/MysqlManager.py
+++ database/MysqlManager.py
@@ -2,22 +2,28 @@ trys = 5
 while True:
     trys -= 1
     if trys == 0:
-        print("Error | Could not install or import all required libarys, you need to fix this before you can continue, maybe reinstall python")
+        print(
+            "Error | Could not install or import all required libarys, you need to fix this before you can continue, maybe reinstall python")
         break
     try:
         from colorama import Fore, Style, init  # pip install colorama
-        import pyperclip #pip install pyperclip
-        import mysql.connector # pip install mysql-connector-python
-        import keyboard # pip install keyboard
-        from dotenv import load_dotenv # pip install python-dotenv
+        import pyperclip  # pip install pyperclip
+        import mysql.connector  # pip install mysql-connector-python
+        import keyboard  # pip install keyboard
+        from dotenv import load_dotenv  # pip install python-dotenv
         from mysql.connector import Error
         from mysql.connector import errorcode
+
         break
     except Exception as e:
         import subprocess
         import time
-        print("Error | Could not import all required librarys, installing all required librarys, please dont touch the new opend cmd")
-        subprocess.run(['start', 'cmd', '/c', 'pip install colorama pyperclip mysql-connector-python keyboard python-dotenv'], shell=True)
+
+        print(
+            "Error | Could not import all required librarys, installing all required librarys, please dont touch the new opend cmd")
+        subprocess.run(
+            ['start', 'cmd', '/c', 'pip install colorama pyperclip mysql-connector-python keyboard python-dotenv'],
+            shell=True)
         time.sleep(10)
 try:
     from datetime import datetime
@@ -36,8 +42,10 @@ try:
     import logging
     import traceback
     import subprocess
+    import json
 except Exception as e:
-    print("Error | could not import all preinstalled librarys, you need to fix this before you can continue, maybe reinstall python")
+    print(
+        "Error | could not import all preinstalled librarys, you need to fix this before you can continue, maybe reinstall python")
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -100,6 +108,7 @@ def ask():
             print(f"{Fore.LIGHTBLACK_EX}( ) Option 4")
             print(f"({Fore.CYAN}X{Style.RESET_ALL}) Option 5")
         time.sleep(0.1)
+
 
 def generate_db():
     def generate_password(length=12):
@@ -181,6 +190,7 @@ def generate_db():
         print(f"Host: {details['host']}")
         print(f"Port: {details['port']}")
 
+
 def CreateStartScript():
     if platform.system() == "Windows":
         FileName = 'start.bat'
@@ -197,10 +207,11 @@ def CreateStartScript():
             with open(FileName, 'w') as file:
                 file.write('python start.py')
 
+
 def Output(Type, Message):
     Now = datetime.now()
     Time = Now.strftime("%H:%M:%S")
-    
+
     if Type.lower() == "info":
         color = Fore.CYAN
     elif Type.lower() == "error":
@@ -217,15 +228,16 @@ def Output(Type, Message):
         color = Fore.LIGHTBLACK_EX
     else:
         color = Fore.WHITE
-    
+
     print(f"{Time} | {color}{Type.capitalize()}{Style.RESET_ALL} | {Message}")
 
-def Clear():
 
+def Clear():
     if platform.system() == "Windows":
         os.system('cls')
     else:
         os.system('clear')
+
 
 def Headder():
     load_dotenv()
@@ -244,8 +256,8 @@ def Headder():
     Output("System", "Running version " + Fore.LIGHTBLACK_EX + os.getenv("Version"))
     print()
 
-def ExecuteAdmin(command):
 
+def ExecuteAdmin(command):
     def IsAdmin():
         try:
             return ctypes.windll.shell32.IsUserAnAdmin()
@@ -261,9 +273,11 @@ def ExecuteAdmin(command):
         Output("Error", "Script is not runnyng by a user with admin rights. some commands may not work")
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
 
+
 def LoadConfics():
     Names = ["lobby", "proxy"]
     return Names
+
 
 def WorkbenchOpen():
     try:
@@ -272,6 +286,7 @@ def WorkbenchOpen():
         print("Error", f"Error opening program")
     except Exception as e:
         Output("Error", f"Error opening program: {e}")
+
 
 def InstallJv():
     Url = "https://download.oracle.com/java/21/latest/jdk-21_windows-x64_bin.msi"
@@ -286,11 +301,12 @@ def InstallJv():
             if 'java version "21' in output:
                 Output("Info", "Java 21 JRE is already installed.")
             else:
-                #Output("Info", "Java 21 JRE is not installed.")
+                # Output("Info", "Java 21 JRE is not installed.")
                 return False
         except FileNotFoundError:
-            #Output(" Info", "Java is not installed.")
+            # Output(" Info", "Java is not installed.")
             return False
+
     Java = check_java_version()
     if Java == "False":
         try:
@@ -306,7 +322,6 @@ def InstallJv():
 
             Clear()
 
-
             subprocess.run(["msiexec", "/i", NewName], check=True)
             os.remove(NewName)
             Clear()
@@ -318,11 +333,12 @@ def InstallJv():
             os.remove(NewName)
             Output("Error", f"Unexpected error: {e}")
 
+
 def InstallDb():
     Url = "https://dev.mysql.com/get/Downloads/MySQLInstaller/mysql-installer-web-community-8.0.40.0.msi"
     OriginalName = "mysql-installer-web-community-8.0.40.0.msi"
     NewName = "mysql-installer.msi"
-    
+
     try:
         if os.path.exists(NewName):
             pass
@@ -336,14 +352,17 @@ def InstallDb():
 
         Clear()
         Output("Info", "Follow this step by step instruction to install mysql database right")
-        
+
         def Tutorial():
             print()
-            Output("Info", "if now you dont see four options: [server], [client], [full], [custom], you have mysql already installed and you decide if you reinstall and follow the tutorial to minimize possible errors or leave it like it is")
+            Output("Info",
+                   "if now you dont see four options: [server], [client], [full], [custom], you have mysql already installed and you decide if you reinstall and follow the tutorial to minimize possible errors or leave it like it is")
             Output("Info", "1. Choose [Custom] installation.")
-            Output("Info", "2. Navigate to MySQL Servers -> MySQL Server -> MySQL Server 8.0 -> MySQL Server 8.0.40 - X64 (or the first option) and click it.")
+            Output("Info",
+                   "2. Navigate to MySQL Servers -> MySQL Server -> MySQL Server 8.0 -> MySQL Server 8.0.40 - X64 (or the first option) and click it.")
             Output("Info", "3. Click the now green arrow to the right between the two selection areas.")
-            Output("Info", "4. Navigate to Applications -> MySQL Workbench -> MySQL Workbench 8.0 -> MySQL Workbench 8.0.40 - X64 (or the first option) and click it.")
+            Output("Info",
+                   "4. Navigate to Applications -> MySQL Workbench -> MySQL Workbench 8.0 -> MySQL Workbench 8.0.40 - X64 (or the first option) and click it.")
             Output("Info", "5. Click the now green arrow to the right between the two selection areas again.")
             Output("Info", "6. Click [Next].")
             Output("Info", "7. Click [Next] again.")
@@ -352,18 +371,21 @@ def InstallDb():
             Output("Info", "10. Click [Execute] and wait for the installation to complete; this may take some time.")
             Output("Info", "11. Click [Next] after the installation.")
             Output("Info", "12. Click [Next] again.")
-            Output("Info", "13. Select [Development Computer] as the config type if not already selected, and leave the rest untouched if you are not sure about the settings.")
+            Output("Info",
+                   "13. Select [Development Computer] as the config type if not already selected, and leave the rest untouched if you are not sure about the settings.")
             Output("Info", "14. Click [Next] again.")
             Output("Info", "15. Choose [Use Strong Password Encryption] if not already selected.")
             Output("Info", "16. Click [Next] again.")
-            Output("Info", "17. If you have never installed any MySQL service before, choose a Root Password and remember it! There is nearly no way to reset it. Otherwise, enter your current root password and click Check.")
+            Output("Info",
+                   "17. If you have never installed any MySQL service before, choose a Root Password and remember it! There is nearly no way to reset it. Otherwise, enter your current root password and click Check.")
             Output("Info", "18. Click [Next] again.")
-            Output("Info", "19. If you want to change something, do it now, but it's not necessary. After that, click [Next] again.")
+            Output("Info",
+                   "19. If you want to change something, do it now, but it's not necessary. After that, click [Next] again.")
             Output("Info", "20. Click [Execute] and wait for the configurations to get updated.")
             Output("Info", "21. After clicking [Finish], you can return here.")
             Output("Successfully", "You have successfully installed MySQL Server!")
             print()
-        
+
         Tutorial()
         subprocess.run(["msiexec", "/i", NewName], check=True)
         time.sleep(1)
@@ -377,6 +399,7 @@ def InstallDb():
     except Exception as e:
         os.remove(NewName)
         Output("Error", f"Unexpected error: {e}")
+
 
 def CreateDatabaseQuestions():
     IpPattern = re.compile(r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
@@ -403,8 +426,6 @@ def CreateDatabaseQuestions():
 
     Clear()
 
-
-
     while True:
         Names = LoadConfics()
         Name = input("Enter Database Name: ")
@@ -418,7 +439,6 @@ def CreateDatabaseQuestions():
             Clear()
             break
 
-
     while True:
         user = input("Enter user, leave blank for root: ")
         if user == "":
@@ -430,13 +450,13 @@ def CreateDatabaseQuestions():
             Clear()
             break
 
-
     while True:
         password = input("Enter password, leave blank for Random: ")
         if password == "":
             Clear()
             Password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(40))
-            Output("Warning", f"The generated Password is {Fore.LIGHTBLACK_EX}{Password}{Style.RESET_ALL}, copy and paste it to the config of the client")
+            Output("Warning",
+                   f"The generated Password is {Fore.LIGHTBLACK_EX}{Password}{Style.RESET_ALL}, copy and paste it to the config of the client")
             Output("System", "Password copyed to clipboard")
             pyperclip.copy(Password)
             input("Press Enter if you copied it ")
@@ -446,8 +466,6 @@ def CreateDatabaseQuestions():
             Password = password
             Clear()
             break
-
-
 
     while True:
         host = input("Enter Host/IP, leave blank for 127.0.0.1 and 'me' for your own IP: ")
@@ -464,7 +482,8 @@ def CreateDatabaseQuestions():
             if MyIp:
                 Clear()
                 Host = host
-                Output("Warning", f"Your IP address is {Fore.LIGHTBLACK_EX}{Host}{Style.RESET_ALL}, copy and paste it to the config of the client")
+                Output("Warning",
+                       f"Your IP address is {Fore.LIGHTBLACK_EX}{Host}{Style.RESET_ALL}, copy and paste it to the config of the client")
                 Output("System", "Ip copyed to clipboard")
                 pyperclip.copy(Host)
                 input("Press Enter if you copied it ")
@@ -481,14 +500,13 @@ def CreateDatabaseQuestions():
                 Clear()
                 Output("Error", "The entered IP is not valid, choose another")
 
-
-    
     while True:
         port = input("Enter port, leave blank for Random: ")
         if port == "":
             Clear()
             Port = ''.join(random.choice(string.digits) for _ in range(4))
-            Output("Warning", f"The generated Port is {Fore.LIGHTBLACK_EX}{Port}{Style.RESET_ALL}, copy and paste it to the config of the client")
+            Output("Warning",
+                   f"The generated Port is {Fore.LIGHTBLACK_EX}{Port}{Style.RESET_ALL}, copy and paste it to the config of the client")
             Output("System", "Port copyed to clipboard")
             pyperclip.copy(Port)
             input("Press Enter if you copied it ")
@@ -513,6 +531,7 @@ def CreateDatabaseQuestions():
     Database = [Name, Host, User, Password, Port]
     Output("System", f"Created Database: {Database}")
 
+
 def Execute(Command):
     Info = ["info", "i", "infoscreen"]
     Cls = ["clear", "cls", "clearscreen", "empty", "space"]
@@ -522,7 +541,7 @@ def Execute(Command):
     Import = ["import", "in", "im", "impo"]
     Export = ["export", "ex", "expo", "out"]
     List = ["list", "ls", "l"]
-    Workbench = ["workbench", "wb", "workb", "wbench"]
+    Workbench = ["workbench", "wb", "workb", "wbench", "work", "bench"]
     Java = ["java", "jre", "jav", "jv"]
 
     if Command in Cls:
@@ -534,7 +553,7 @@ def Execute(Command):
         Clear()
         Headder()
         Output("Last", "Entered " + Command)
-        
+
         parts = Command.lower().split()
 
         if len(parts) >= 2:
@@ -567,10 +586,12 @@ def Execute(Command):
         else:
             Output("Error", "Please enter a command in the format: <category> <action> <service> if service needed")
 
+
 def ExecuteAtStart():
     CreateStartScript()
     Clear()
     Headder()
+
 
 ExecuteAtStart()
 
@@ -583,6 +604,8 @@ if __name__ == "__main__":
                 Clear()
                 time.sleep(0.1)
                 break
+            elif Command == "":
+                time.sleep(0.1)
             else:
                 Execute(Command)
         except Exception as e:
