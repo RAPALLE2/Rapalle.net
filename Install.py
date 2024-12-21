7716449d990b214d23c04af6fe2f80e619ae7a30
RAPALLE2 <minecrafter_tobi@web.de>
2024-12-21 23:38:04 +0100

    Install.py added clear befor download process

--- Install.py
+++ Install.py
@@ -169,6 +169,7 @@ if __name__ == "__main__":
     while True:
         try:
             selected = ask()
+            Clear()
             if selected == 1:
                 Java()
             elif selected == 2:
