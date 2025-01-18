836dc5134f5a60e20f3ee448d2d12555f2d3ac70
RAPALLE2 <minecrafter_tobi@web.de>
2025-01-18 16:05:47 +0100

    Imported old cloud under cloud/Cloud and updated to polocloud-1.0.0.6

---
+++ cloud/Cloud/cloud-start.bat
@@ -0,0 +1,2 @@
+@echo off
+java -jar ./polocloud-1.0.0.6-SNAPSHOT.jar
--- logs/test_files/test.py
+++ logs/test_files/test.py
@@ -1,69 +1,119 @@
-#1st vars:
-#integer (numbers)
-var1 = 3
-#boolean (true/false)
-var2 = True # False
-#string (text)
-var3 = "hello"
+##1st vars:
+##integer (numbers)
+#var1 = 3
+##boolean (true/false)
+#var2 = True # False
+##string (text)
+#var3 = "hello"
+#
+#var4 = input("gib deinen namen an: ")
+#
+##var mixing
+#one = 5
+#two = 4
+#summ = one + two # = 9
+#
+##for integer instead of trys = trys - 1, do trys -= 1 or instead of count = count + 1, do count += 1p
+#
+#name = "hans"
+#text = "hello, "
+#result = text + name # = hello, hans
+## or
+#string = "hello"
+#out = string + ", " + name
+#
+## same for print
+##1. print(text + name)
+##2. print(string + ", " + name)
+#
+#
+##2nd if, try, while
+##if:
+#if var1 == 3:
+#    pass
+## at integer also works ==, <, >, >=, <=
+#if var2 == True:
+#    pass
+##or
+#if var2:
+#    pass
+##because its true it dont need to look if its true
+#if var3 == "hello":
+#    pass
+##or
+#if var3 == name:
+#    pass
+#
+##elif if it should end after first right option
+##else for if nothing above was the right option
+#
+##try
+#try:
+#    pass
+#except:
+#    pass
+##try is if something could went wrong so not the programm closes but it does the stuff in except good for imports that have to get installed by pip
+#
+##while
+#while True:
+#    pass
+## runs forever
+#
+#trys = 5
+#while trys > 0:
+#    trys = trys - 1
+##or
+#while True:
+#    if trys > 0:
+#        trys = trys - 1
+#    else:
+#        break
 
-#var mixing
-one = 5
-two = 4
-summ = one + two # = 9
-
-#for integer instead of trys = trys - 1, do trys -= 1 or instead of count = count + 1, do count += 1p
-
-name = "hans"
-text = "hello, "
-result = text + name # = hello, hans
-# or
-string = "hello"
-out = string + ", " + name
-
-# same for print
-#1. print(text + name)
-#2. print(string + ", " + name)
 
 
-#2nd if, try, while
-#if:
-if var1 == 3:
-    pass
-# at integer also works ==, <, >, >=, <=
-if var2 == True:
-    pass
-#or
-if var2:
-    pass
-#because its true it dont need to look if its true
-if var3 == "hello":
-    pass
-#or
-if var3 == name:
-    pass
 
-#elif if it should end after first right option
-#else for if nothing above was the right option
 
-#try
-try:
-    pass
-except:
-    pass
-#try is if something could went wrong so not the programm closes but it does the stuff in except good for imports that have to get installed by pip
 
-#while
-while True:
-    pass
-# runs forever
 
-trys = 5
-while trys > 0:
-    trys = trys - 1
-#or
-while True:
-    if trys > 0:
-        trys = trys - 1
-    else:
-        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import sys; print('Python %s on %s' % (sys.version, sys.platform))
+sys.path.extend(["C://"])
+
+
+
+var = input("gib deinen namen an: ")
+print(var)
+
+
+
+
+
+
+
+
+
+
+
 
