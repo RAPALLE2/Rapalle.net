3eb4c9cbe1ba217b644c697c925fbf56ea5f5693
RAPALLE2 <minecrafter_tobi@web.de>
2025-07-27 00:06:12 +0200

    added main.py as template

---
+++ python/main.py
@@ -0,0 +1,67 @@
+import colorama
+
+from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import NestedCompleter
+from prompt_toolkit.styles import Style
+
+# Stack-Kommandos mit Variablen
+completer = NestedCompleter.from_nested_dict({
+    "service": {
+        "web": {
+            "host": None,
+            "log": None
+        },
+        "git": None,
+        "cloud": None,
+        "network": None
+    },
+    "exit": None
+})
+
+print("      ______  ___  ______  ___   _      _      _____  _   _  _____ _____")
+print("      | ___ \\/ _ \\ | ___ \\/ _ \\ | |    | |    |  ___|| \\ | ||  ___|_   _|")
+print("      | |_/ / /_\\ \\| |_/ / /_\\ \\| |    | |    | |__  |  \\| || |__   | |")
+print("      |    /|  _  ||  __/|  _  || |    | |    |  __| | . ` ||  __|  | |")
+print("      | |\\ \\| | | || |   | | | || |____| |____| |____| |\\  || |___  | |")
+print("      \\_| \\_\\_| |_/\\_|   \\_| |_/\\_____/\\_____/\\____(_)_| \\_/\\____/  \\_/")
+print("      Made by Rapalle            Automatic Server Management Software")
+print("      Version "+colorama.Fore.LIGHTBLACK_EX+"1.0.0")
+
+while True:
+
+    completer = NestedCompleter.from_nested_dict({
+        "exit": None,
+        "service": None
+    })
+
+    style = Style.from_dict({'prompt': '#888888'})
+
+    command = PromptSession().prompt([('class:prompt', ' >> ')], completer=completer, style=style)
+
+    parts = command.split(" ")
+    if parts[0] in ("exit", "stop", "break", "quit"): break
+    elif parts[0] == "service":
+        if parts[1] == "web":
+            if parts[2] == "host": pass
+            elif parts[2] == "log": pass
+        elif parts[2] == "git":
+            if parts[3] == "": pass
+            elif parts[3] == "": pass
+        elif parts[2] == "cloud":pass
+        elif parts[2] == "network":pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
